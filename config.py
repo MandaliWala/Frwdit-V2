ef6867dc799410c5d549bc47744ec106ad5f0770
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-08-13 21:15:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,6 +14,6 @@ class Config:
     CAPTION = os.environ.get("CAPTION", "")
     FILTER_TYPE = os.environ.get("FILTER_TYPE", "document")
     OWNER_ID = os.environ.get("OWNER_ID", 6302921275)
-    SESSION = os.environ.get("SESSION")
+    SESSION = os.environ.get("SESSION", "BQF-avYAd0_dPxKuLkyIVEBqhFuG3YXNOgTiNaXmPDXPrB_K6sHa3Y2ZMxKEcipBxnwBkXfmLO5uiP2-LNmT5YJ19wLWLtJb8ZCKsFGGWBEKaNwe1BuYme1gIhm0nq9F2mqhjvSsXsWz1nc-YBBamBlbGuiiSVVgp1wLZl69HQDQukJ0_RSpREX4seW4rczvI4V1PG3-jv3i1Zm8J6N6NyjgejyC-Gx_NCH1fiqqhz-1kkWKBzHDhKsFbc90p8BGN9a99ruwYgTbsXeReHtGm9Wi6av5dgsAc40Zw9GzHB4ID6L1DEDefbYJoFymvrUIoJOIAr7HkScLH5TiiCjpuJAtK3C3-AAAAAGIMzILAA")
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
